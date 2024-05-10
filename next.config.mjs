27767ffaf97349e1d3bec51893fc1da25836e2c5
ndambodavid <linmik420@gmail.com>
2024-05-10 18:12:22 +0300

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // sassOptions: {
+    //     includePaths: [path.join(__dirname, 'styles')],
+    //   },
+    //   images: {
+    //     domains: ['res.cloudinary.com', 'media.dev.to']
+    //   }
+    images: {
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'media.dev.to'
+            }
+        ]
+    }
+};
 
 export default nextConfig;

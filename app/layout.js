cb2a631f9bde836c33c49e53eb210e70db0dce3c
ndambodavid <davidndambo6@gmail.com>
2024-05-27 16:22:33 +0300

    desc update

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Ndambo, The Dev - Software Developer',
-  description: 'This is the portfolio of David Ndmabo. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
+  description: 'This is the portfolio of David Ndmabo. I am a fullstack professional software developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
 };
 
 export default function RootLayout({ children }) {

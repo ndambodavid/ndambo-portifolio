cfca61c48d4234afb3aead23d9f40f72652c1ac3
ndambodavid <davidndambo6@gmail.com>
2024-05-31 14:34:40 +0300

    desc update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "David Ndambo",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skills are based on TypeScript, Java and Python and I love to do most of the things using this Stack. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I am a professional, flexible and enthusiastic software developer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skills are based on TypeScript, Java, Laravel and Python and I love to do most of the things using this Stack. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'davidndambo6@gmail.com',
   phone: '+254793914286',
   address: 'Nairobi, Kenya',

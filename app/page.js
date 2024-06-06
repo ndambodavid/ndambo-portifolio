e4768499c3d6815171c76fde7803639091b1745a
ndambodavid <davidndambo6@gmail.com>
2024-06-06 16:17:09 +0300

    remove contributions section

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default async function Home() {
       />
       <GitLanguage />
       <Rank />
-      <Contributions />
+      {/* <Contributions /> */}
       <Education />
       {/* <Blog blogs={blogs} /> */}
       <ContactSection />

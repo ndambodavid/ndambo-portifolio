40457238959a07e8cbe2fae406cac324e4d1e1c0
ndambodavid <linmik420@gmail.com>
2024-09-28 15:09:17 +0300

    git projects update

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -28,7 +28,7 @@ const Projects = ({ projects, profile }) => {
       </div> */}
       <div className="grid py-12 grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
         {
-          projects.map(project => (
+          projects.slice(0, 8).map(project => (
             <GlowCard key={project.id} identifier={`project-${project.id}`}>
               <ProjectCard project={project} />
             </GlowCard>
